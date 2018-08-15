06732e79c5a4ecd4371f9e5a6f23823027cd8b18
Billie Simmons <billiesnyc@gmail.com>
2018-08-15 13:58:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     elsif array.length == 2 
     array.join(" and ")
     elsif array.length >= 3
-    last_item = array.last
+    last_item = array.pop
     array.push("and #{last_item}")
     array.join(", ")
   end
